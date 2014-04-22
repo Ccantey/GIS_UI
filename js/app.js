5714bee6bfdcf5ca2843b5889cd048457db0a050
Chris Cantey <cantey0701@gmail.com>
2014-04-22 11:11:59 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+$("#menu-close").click(function(e) {
+	e.preventDefault();
+	$("#sidebar-wrapper").toggleClass("active");
+ });
+  
+  
+$("#menu-toggle").click(function(e) {
+e.preventDefault();
+$("#sidebar-wrapper").toggleClass("active");
+});
+
+$('.radioset').buttonset();
+
+});
