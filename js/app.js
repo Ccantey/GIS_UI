0f2344e1cd8956b3678955c9e0d9d02552585d33
Chris Cantey <cantey0701@gmail.com>
2014-05-14 16:28:06 -0500

    Move Esri zoom slider around

--- js/app.js
+++ js/app.js
@@ -3,16 +3,21 @@ $(document).ready(function () {
 $("#menu-close").click(function(e) {
 	e.preventDefault();
 	$("#sidebar-wrapper").toggleClass("active");
+	  $('#map-zoom-slider,.esriSimpleSlider,.foot').animate({ 'left': '10px' }, 500, 'easeOutQuad');
  });
   
   
 $("#menu-toggle").click(function(e) {
 e.preventDefault();
 $("#sidebar-wrapper").toggleClass("active");
+$('#map-zoom-slider,.esriSimpleSlider,.foot').animate({ 'left': '360px' }, 500, 'easeInQuad');
 });
 
 $('.radioset').buttonset();
 
 $('a[data-toggle="tooltip"]').tooltip();
+
+
+
 
 });
