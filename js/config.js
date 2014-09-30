0856a0f8b137b58fd52b0c4e94941a780c0faa41
Chris Cantey <cantey0701@gmail.com>
2014-09-30 08:44:29 -0500

    Add Wisconsin Rapids Services

--- js/config.js
+++ js/config.js
@@ -1,1 +1,19 @@
-var vectorBaseUrl = "http://gis.wirapids.org/arcgis/rest/services/VectorBasemap2/MapServer";
+var mapServiceURL = "http://gis.wirapids.org/arcgis/rest/services/DynamicBasemap/MapServer";
+//var featureServiceURL = "http://gis.wirapids.org/arcgis/rest/services/DynamicBasemap/FeatureServer";
+var aerialServiceUrl = "http://gis.wirapids.org/arcgis/rest/services/AerialBasemap/MapServer";
+var vectorBaseUrl = "http://gis.wirapids.org/arcgis/rest/services/VectorBasemapHybrid/MapServer";
+
+var gsvcURL = "http://gis.wirapids.org/arcgis/rest/services/Utilities/Geometry/GeometryServer";      //url to geometry service
+
+var printServiceURL = "http://gis.wirapids.org/arcgis/rest/services/ExportWebMap/GPServer/Export%20Web%20Map";
+// var mailLabelUrl = "https://gis.wirapids.org/CityViewer/outputs/mail Labels.pdf";
+var csvUrl = "http://gis.wirapids.org/CityViewer/outputs/parcelInfo.csv";
+
+var utilityLayerID = [-1];
+var parcelLayerID = 27;     
+
+// var link = "gis.wirapids.org/CityViewer/Asbuilts/";
+var mailLink = "https://gis.wirapids.org/CityViewer/outputs/";
+
+
+ var identifyLayerAdditional = [];
