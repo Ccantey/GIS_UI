1ac8370df51187d37ae53e8e5e691c4144581239
Chris Cantey <cantey0701@gmail.com>
2014-10-08 13:47:25 -0500

    Remove reference to map services-AMD style bitches

--- js/config.js
+++ js/config.js
@@ -1,11 +1,4 @@
-var mapServiceURL = "http://gis.wirapids.org/arcgis/rest/services/DynamicBasemap/MapServer";
-//var featureServiceURL = "http://gis.wirapids.org/arcgis/rest/services/DynamicBasemap/FeatureServer";
-var aerialServiceUrl = "http://gis.wirapids.org/arcgis/rest/services/AerialBasemap/MapServer";
-var vectorBaseUrl = "http://gis.wirapids.org/arcgis/rest/services/VectorBasemapHybrid/MapServer";
 
-var gsvcURL = "http://gis.wirapids.org/arcgis/rest/services/Utilities/Geometry/GeometryServer";      //url to geometry service
-
-var printServiceURL = "http://gis.wirapids.org/arcgis/rest/services/ExportWebMap/GPServer/Export%20Web%20Map";
 // var mailLabelUrl = "https://gis.wirapids.org/CityViewer/outputs/mail Labels.pdf";
 var csvUrl = "http://gis.wirapids.org/CityViewer/outputs/parcelInfo.csv";
 
