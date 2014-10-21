874b155ed190474a178da92771c7d023b7410c8d
Chris Cantey <cantey0701@gmail.com>
2014-10-21 13:22:13 -0500

    Update and rename helper.js to init.js

---
+++ js/init.js
@@ -0,0 +1,277 @@
+var initExtent, identifyTask, identifyParams, gsvc;
+var identifyTaskParcel, identifyParamsParcel, params;
+var basemap, utilityMap, graphicLayer, graphicLayerLabels, tempGraphicLayer, maxExtent, printOptions;
+var app, measure;
+var iPoint;
+var A_Drawings;
+var overlayLayer;
+var aerialLayer;
+var operationToDo;
+var legend = null;
+var legendLayers = [];
+var iTool = null;
+var navToolbar = null; 
+var measureTool = null; 
+var drawingTool = null;
+var editToolbar = null;
+var map = null;
+var toolbar = null;
+var symbols = {};
+var printTask = null;   
+var ctxMenuForGraphics, ctxMenuForMap;
+var printAreaGraphic = null;
+var globalMapLayers = {};
+var geometryDraw = [];
+var geometryBuffer = [];
+var selectedFeatures = { features: [] };
+var unselectedGeometry = { features: [] };
+var symbolDraw;
+var attrAll = [];
+var labelling = false;
+var attValues = [];
+var csvInfo = [];
+var mailParcels = [];
+var measureGraphic = null;
+var measureGeometry = null;
+var multipleIdentifyStack = [];
+var multipleIdentifyLayerName = [];
+var navEvent;
+
+function createCSVFile() {
+    $.post('outputs/csvCreate.php', { q: csvInfo }, function (data) {  });
+}
+  define(["dojo/ready","esri/urlUtils", "dojo/dom", "dojo/on", "esri/domUtils", "esri/map", "config/commonConfig", "app/identification", "app/checktree", "app/handlers","app/measurements","app/mapNav", "esri/request", "esri/tasks/PrintTask", "esri/dijit/Legend", "esri/toolbars/draw", "esri/toolbars/edit", "esri/tasks/GeometryService", "esri/tasks/BufferParameters", "esri/dijit/editing/AttachmentEditor", "esri/layers/ArcGISTiledMapServiceLayer", "esri/layers/ArcGISDynamicMapServiceLayer", "esri/layers/FeatureLayer", "esri/layers/GraphicsLayer", "esri/geometry/Extent", "esri/SpatialReference", "dojo/domReady!"],
+           function(ready, urlUtils, dom, on, domUtils, Map, config, identification, checktree, handlers, Measurement, mapNav, esriRequest, PrintTask, Legend, Draw, Edit, GeometryService, BufferParameters, AttachmentEditor, ArcGISTiledMapServiceLayer, ArcGISDynamicMapServiceLayer, FeatureLayer, GraphicsLayer, Extent, SpatialReference){
+		   
+		    ready(function(){
+            /*urlUtils.addProxyRule({
+                urlPrefix: "http://gis.wirapids.org",
+                proxyUrl: "http://gis.wirapids.org/proxy/proxy.php"
+            }); */           
+
+             initExtent = new Extent({"xmin":-10014198.126251305,"ymin":5518475.931924282,"xmax":-9988152.208863167,"ymax":5533954.430152008,spatialReference:{"wkid":102100}});
+             map = new Map("map", {
+                extent: initExtent  
+             }); 
+             
+
+            //overlayLayer.url = config.mapServices.dynamic;
+            //utilityMap.url = config.mapServices.dynamic;
+            gsvc = new GeometryService(config.helperServices.geometry); //identification, doBuffer, measureUpdate
+            printTask = new PrintTask(config.helperServices.print);
+
+            // this is comparable to "dojo.connect(map, 'onLoad', function () {" but doesn't do anything yet.
+            on(map, "load", function(){
+/*                editToolbar = new Edit(map);
+                on(map, "click", function (evt) {
+                  editToolbar.deactivate();
+                });
+                createMapMenu();
+	            createGraphicsMenu();*/
+			  
+                legendLayers.push({ layer: utilityMap, title: "Legend" });
+                legendLayers.push({ layer: overlayLayer, title: "Legend ",hideLayers:[0,1,2,3,27, 28, 29, 31]});
+                legend = new Legend({
+                  map:map,
+                  layerInfos:legendLayers,
+                  autoUpdate: true,
+                },"legendDiv");
+                legend.startup();
+
+                var treeLegend = new checktree();
+                var utilities = $(".overlayLayer, .utilityLayer, .tree");
+                var basemaplayers = $(".overlayLayer");
+                on(utilities, 'change', function(){
+                      treeLegend._treeGetUtilityLayers();
+                } );
+                on(basemaplayers, 'change', function(){
+                    treeLegend._treeGetOverlayLayers();
+                } );
+            });
+
+            basemap = new ArcGISTiledMapServiceLayer(config.mapServices.vector); 
+            map.addLayer(basemap);
+
+            aerialLayer = new ArcGISTiledMapServiceLayer(config.mapServices.aerial, {maxScale: 1128}); //scale to fit, rather than 1128.497
+            map.addLayer(aerialLayer);
+            aerialLayer.hide();
+            //Add Feature service (A_Drawings/Attachments)
+            A_Drawings = new FeatureLayer("http://gis.wirapids.org/arcgis/rest/services/DynamicBasemap/FeatureServer/24", {
+              mode: FeatureLayer.MODE_ONDEMAND,
+              outFields:["*"]
+            });
+            map.addLayers(A_Drawings);
+
+            attachmentEditor = new AttachmentEditor({}, dom.byId("content"));
+            attachmentEditor.startup();
+
+            A_Drawings.hide();
+            //Add dynamic map 
+            overlayLayer = new ArcGISDynamicMapServiceLayer(config.mapServices.dynamic, {id:"Basemap"});
+            overlayLayer.setImageFormat("png32");
+            overlayLayer.setVisibleLayers([1,27,28,29]);
+            map.addLayer(overlayLayer);
+			
+            utilityMap = new ArcGISDynamicMapServiceLayer(config.mapServices.dynamic, { id: "Utility" });
+            utilityMap.setImageFormat("png32");
+            utilityMap.setVisibleLayers([-1]);
+            map.addLayer(utilityMap);		
+			
+            //Keep map in viewer
+            maxExtent = map.extent;
+            on(map, "extent-change", function (initExtent){
+                printOptions._changePrintGraphic(); //move print box if its on.
+                var adjustedEx = new Extent(initExtent.extent.xmin, initExtent.extent.ymin, initExtent.extent.xmax, initExtent.extent.ymax, new SpatialReference({ wkid:102100 }));
+                var flag = false; 
+                //set a buffer to make the max extent a slightly bigger to void minor differences
+                //the map unit for this case is meter. 
+                var buffer = 25000;
+        
+                var onLoadHandle = on(basemap, "update", function(){
+                    onLoadHandle.remove();
+                    if(initExtent.extent.xmin < maxExtent.xmin-buffer) {
+                        adjustedEx.xmin = maxExtent.xmin;
+                        adjustedEx.xmax = Math.abs(initExtent.extent.xmin - maxExtent.xmin) + initExtent.extent.xmax;
+                        flag = true;
+                    }
+                    if(initExtent.extent.ymin < maxExtent.ymin-buffer) {
+                        adjustedEx.ymin = maxExtent.ymin;
+                        adjustedEx.ymax = Math.abs(initExtent.extent.ymin - maxExtent.ymin) + initExtent.extent.ymax;
+                        flag = true;
+                    }
+                    if(initExtent.extent.xmax-buffer > maxExtent.xmax) {
+                        adjustedEx.xmax = maxExtent.xmax;
+                        adjustedEx.xmin =initExtent.extent.xmin - Math.abs(initExtent.extent.xmax - maxExtent.xmax);
+                        flag = true;
+                    }
+                    if(initExtent.extent.ymax-buffer > maxExtent.ymax) {
+                        adjustedEx.ymax = maxExtent.ymax;
+                        adjustedEx.ymin =initExtent.extent.ymin - Math.abs(initExtent.extent.ymax - maxExtent.ymax);
+                        flag = true;
+                    }
+                    if (flag === true) {
+                        map.setExtent(adjustedEx);        
+                    }
+                    flag = false;
+                }); 
+                
+                var currentScale=map.getScale();
+                if(currentScale > 4800){
+                    $("#AddLabels, #DimLabels, #PidLabels").prop('indeterminate', true).change();
+                    $("#AddLabels, #DimLabels, #PidLabels").prop('disabled', true).change();
+                    $("#AddLabels, #DimLabels, #PidLabels").parent().css("color", "#777");
+                }else{
+                    $("#AddLabels, #DimLabels, #PidLabels").prop('indeterminate', false).change();
+                    $("#AddLabels, #DimLabels, #PidLabels").prop('disabled', false).change();
+                    $("#AddLabels, #DimLabels, #PidLabels").parent().css("color", "#FFF");
+                }
+                if (currentScale > 9600){
+                    $("#Contours, #Adraw").prop('indeterminate', true).change();
+                    $("#Contours, #Adraw").prop('disabled', true).change();
+                    $("#Contours, #Adraw").parent().css("color", "#777")    
+                }else{
+                    $("#Contours, #Adraw").prop('indeterminate', false).change();
+                    $("#Contours, #Adraw").prop('disabled', false).change();
+                    $("#Contours, #Adraw").parent().css("color", "#FFF")        
+                }           
+            });
+
+            on(map, "update-start", function(){
+                domUtils.show(loading);
+            });
+           
+            on(map, "update-end", function(){
+                domUtils.hide(loading);
+                map.disableDoubleClickZoom();
+            });            
+
+            on(map, "zoom-end", function () {
+                $("#navigation input").attr("checked", false).button("refresh");
+                var adjustedScale = map.getScale();
+                if (adjustedScale > 72226) {
+                    map.setExtent(initExtent);
+                }
+            });
+            graphicLayer = new GraphicsLayer();
+            map.addLayer(graphicLayer);
+
+            tempGraphicLayer = new GraphicsLayer();
+            map.addLayer(tempGraphicLayer);
+
+            graphicLayerLabels = new GraphicsLayer();
+            map.addLayer(graphicLayerLabels);
+
+            //buffer params
+            params = new BufferParameters();
+            params.bufferSpatialReference = new SpatialReference({ wkid: 103734 });
+          
+            operationToDo = 'identify';
+            iTool = new Draw(map, { showTooltips: false });
+            //iTool.activate(Draw.POINT); //use _geometryType to make iTool methods global
+            iTool.activate(iTool._geometryType='point');
+            app = new identification();
+            measure = new Measurement();
+            iTool.on("draw-end", function (evt) {
+              switch (operationToDo){
+              case "identify":
+                  app._identify(evt.geometry);
+                  break;
+              case "selection":
+                  app._drawGraphic(evt.geometry);
+                  break;
+              case "measure":
+                  measure._getAreaAndLength(evt.geometry);
+                  break;
+              case "drawing":
+                  app._addDrawingToMap(evt.geometry)
+                  break;
+               }
+            });
+
+            var searchButton = $('#searchTab button');
+            on(searchButton, 'click', function(){
+              $('.results.identify').hide();
+              if (this.id == 'ownerGo') {
+              //alert('dadfs');
+                app._searchParcelByAttribute("Owner");
+              } else if (this.id == "addressGo") {
+                app._searchParcelByAttribute("Address");
+              } else {
+                app._searchParcelByAttribute("PID");
+              }
+            });
+
+            var searchBoxes = $('#owner, #addresses, #pid');
+            on(searchBoxes, 'keypress', function(event){
+              if(this.id == 'owner'){
+                  if(event.keyCode == 13){
+                      app._searchParcelByAttribute('Owner');
+                  }
+              } else if (this.id == "addresses") {
+                  if(event.keyCode == 13){
+                      app._searchParcelByAttribute('Address');
+                  }
+              } else if (this.id == "pid") {
+                  if(event.keyCode == 13){
+                      app._searchParcelByAttribute('PID');
+                  }
+              }
+            });
+
+            //esriRequest is a utility method to retrieve data from a web server. Data can be static (stored in a file on the web server), or it can be dynamic (generated on-demand by a web service). esriRequest can handle the following formats: plain txt, xml, json, jsonp
+            var layersRequest = esriRequest({
+                url: config.mapServices.dynamic,
+                content: { f: "json" },
+                handleAs: "json",
+                callbackParamName: "callback"
+            });
+            layersRequest.then(
+                function(response) {
+                    //console.log("Success: ", response.layers);
+                    globalMapLayers = response.layers;
+                }, function(error) {
+                    console.log("Error: ", error.message);
+                });
+				
+        }); //end ready
+    }); //end require
